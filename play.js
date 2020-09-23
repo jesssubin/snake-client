4ef9add6af5683f884fba3d4248febbb98bc4642
jesssubin <jess.s.moon@gmail.com>
2020-09-23 23:19:51 +0000

    message added

--- play.js
+++ play.js
@@ -1,30 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input'); 
 console.log('Connecting ...');
-connect();
+setupInput(connect());
 
-const handleUserInput = function (key) {
-  // console.log(key); 
-  if(key === "w"){
-    //going up instruction to the server 
-  }
-  if(key === "\u0003"){
-    //stop the program 
-    process.exit(); 
-  }
-}
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-
-  return stdin;
-}
-setupInput(); 
 
