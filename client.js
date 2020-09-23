4c1ef6a0c40299f37e880bf1cd3b5ab6ee98f869
jesssubin <jess.s.moon@gmail.com>
2020-09-23 17:54:23 +0000

    added name

--- client.js
+++ client.js
@@ -6,11 +6,17 @@ const net = require('net');
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
-    port: 50541
+    port: 50541, 
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  
+  conn.on('connect', () => {
+    conn.write("Name: MSB")
+    console.log("Connected succesfully")})
+//received data from server and console.logging it 
   conn.on('data', (data) => console.log(data))
+  // conn.on('data', () => console.log("Successfully connected to game server"))
   return conn;
 }
 
